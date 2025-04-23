dfd320ac7f56fab039d151aad7bff8366b37a785
kevinmitchell36 <kevingm31@gmail.com>
2025-04-23 11:31:47 -0500

    Adds practice dynamic rendering by changing a buzz word in the subtitle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
+const buzzWords = ['Fundamental', 'Core', 'Crucial']
+
+function randomBuzz(max) {
+  const word = Math.floor(Math.random() * (max + 1))
+  return word
+}
+
 
 function Header() {
   return (
-    
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {buzzWords[randomBuzz(buzzWords.length)]} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
